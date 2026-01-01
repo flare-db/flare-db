3d66eb469f2ada66c50d424d885e14f40289b902
Ganesh <ganeshsivakumar85@gmail.com>
2026-01-01 11:26:50 +0530

    vendor Apache Beam model protos and Rust types (#2)
    * apache beam model
    
    * beam proto rust codes

---
+++ beam-model-rs/src/v1/mod.rs
@@ -0,0 +1,48 @@
+pub mod org {
+    pub mod apache {
+        pub mod beam {
+            pub mod model {
+
+                pub mod pipeline {
+                    pub mod v1 {
+                        include!("org.apache.beam.model.pipeline.v1.rs");
+                    }
+                }
+
+                pub mod fn_execution {
+                    pub mod v1 {
+                        include!("org.apache.beam.model.fn_execution.v1.rs");
+                    }
+                }
+
+                pub mod job_management {
+                    pub mod v1 {
+                        include!("org.apache.beam.model.job_management.v1.rs");
+                    }
+                }
+
+                pub mod expansion {
+                    pub mod v1 {
+                        include!("org.apache.beam.model.expansion.v1.rs");
+                    }
+                }
+
+                pub mod interactive {
+                    pub mod v1 {
+                        include!("org.apache.beam.model.interactive.v1.rs");
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub use org::apache::beam::model::pipeline::v1::*;
+
+pub use org::apache::beam::model::fn_execution::v1::*;
+
+pub use org::apache::beam::model::job_management::v1::*;
+
+pub use org::apache::beam::model::expansion::v1::*;
+
+pub use org::apache::beam::model::interactive::v1::*;
---
+++ beam/model/BEAM_VERSION.md
@@ -0,0 +1,6 @@
+## Apache Beam model protos
+
+
+## Commit - Dec 27, 2025
+- Source: https://github.com/apache/beam
+- Beam Version: 2.70.0
