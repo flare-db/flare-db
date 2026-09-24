6db6c9579e28fc04e9ff566f33581e946dd7a8f6
ganesh-skumar <ganesh.gsivakumar@gmail.com>
2026-09-24 15:02:14 +0530

    update example and gradle

--- example/flatten/src/main/java/com/flaredb/example/FlattenExample.java
+++ example/flatten/src/main/java/com/flaredb/example/FlattenExample.java
@@ -5,101 +5,55 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.TypeDescriptors;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.transforms.GroupByKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FlattenExample {
 
-       private static final Logger LOG = LoggerFactory.getLogger(FlattenExample.class);
+  private static final Logger LOG = LoggerFactory.getLogger(FlattenExample.class);
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        FlattenExamplePipelineOptions options =
-                        FlattenExamplePipelineOptions.applyFlareDefaults(
-                                PipelineOptionsFactory.fromArgs(args)
-                                        .as(FlattenExamplePipelineOptions.class));
-Pipeline pipeline = Pipeline.create(options);
+    FlattenExamplePipelineOptions options =
+        FlattenExamplePipelineOptions.applyFlareDefaults(
+            PipelineOptionsFactory.fromArgs(args).as(FlattenExamplePipelineOptions.class));
+    Pipeline pipeline = Pipeline.create(options);
 
-                            PCollection<KV<String, Integer>> inputA =
-                                pipeline
-                                    .apply("CreateA",
-                                        Create.of(
-                                            KV.of("a", 1),
-                                            KV.of("b", 2)))
-                                    .apply(
-                                        "PrepareA",
-                                        MapElements.into(
-                                                TypeDescriptors.kvs(
-                                                    TypeDescriptors.strings(),
-                                                    TypeDescriptors.integers()))
-                                            .via(kv -> KV.of(kv.getKey(), kv.getValue() * 10)));
+    PCollection<KV<String, Integer>> storeSales =
+        pipeline.apply("StoreSales", Create.of(KV.of("apple", 10), KV.of("banana", 20)));
 
+    PCollection<KV<String, Integer>> onlineSales =
+        pipeline.apply("OnlineSales", Create.of(KV.of("apple", 15), KV.of("banana", 25)));
 
-                            PCollection<KV<String, Integer>> inputB =
-                                pipeline
-                                    .apply("CreateB",
-                                        Create.of(
-                                            KV.of("a", 3),
-                                            KV.of("b", 4)))
-                                    .apply(
-                                        "PrepareB",
-                                        MapElements.into(
-                                                TypeDescriptors.kvs(
-                                                    TypeDescriptors.strings(),
-                                                    TypeDescriptors.integers()))
-                                            .via(kv -> KV.of(kv.getKey(), kv.getValue() * 10)));
+    PCollection<KV<String, Integer>> partnerSales =
+        pipeline.apply("PartnerSales", Create.of(KV.of("apple", 5), KV.of("banana", 10)));
 
+    PCollection<KV<String, Integer>> allSales =
+        PCollectionList.of(storeSales)
+            .and(onlineSales)
+            .and(partnerSales)
+            .apply("FlattenSales", Flatten.pCollections());
 
-                            PCollection<KV<String, Integer>> inputC =
-                                pipeline
-                                    .apply("CreateC",
-                                        Create.of(
-                                            KV.of("a", 5),
-                                            KV.of("b", 6)))
-                                    .apply(
-                                        "PrepareC",
-                                        MapElements.into(
-                                                TypeDescriptors.kvs(
-                                                    TypeDescriptors.strings(),
-                                                    TypeDescriptors.integers()))
-                                            .via(kv -> KV.of(kv.getKey(), kv.getValue() * 10)));
+    PCollection<KV<String, Iterable<Integer>>> salesByProduct =
+        allSales.apply("GroupByProduct", GroupByKey.create());
 
+    salesByProduct.apply(
+        "Print",
+        ParDo.of(
+            new DoFn<KV<String, Iterable<Integer>>, Void>() {
+              @ProcessElement
+              public void processElement(ProcessContext context) {
+                KV<String, Iterable<Integer>> element = context.element();
 
-                            PCollection<KV<String, Integer>> flattened =
-                                PCollectionList.of(inputA)
-                                    .and(inputB)
-                                    .and(inputC)
-                                    .apply("Flatten", Flatten.pCollections());
+                LOG.info("product={} sales={}", element.getKey(), element.getValue());
+              }
+            }));
 
-
-                            PCollection<KV<String, Iterable<Integer>>> grouped =
-                                flattened.apply(
-                                    "GBK",
-                                    GroupByKey.create());
-
-                            grouped.apply(
-                                "Print",
-                                ParDo.of(
-                                    new DoFn<KV<String, Iterable<Integer>>, Void>() {
-
-                                      @ProcessElement
-                                      public void processElement(ProcessContext context) {
-                                        KV<String, Iterable<Integer>> element = context.element();
-
-                                        LOG.info(
-                                            "key={} values={}",
-                                            element.getKey(),
-                                            element.getValue());
-                                      }
-                                    }));
-
-                            pipeline.run();
-    }
+    pipeline.run();
+  }
 }
